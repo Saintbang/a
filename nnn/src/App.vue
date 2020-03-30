cee86894de3d168e1d77ee3acc1d195e28cbb150
wangbangbnag <1516876722@qq.com>
2020-03-30 23:41:56 +0800

    nnn

---
+++ nnn/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <button @click="get()">点我</button>
+    </div>
+</template>
+
+<script>
+import fs from "fs"
+export default {
+  name: "app",
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    get(){
+      fs.readFile('./components.a', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
+    }
+  },
+  components: {},
+  mounted() {
+  }
+};
+</script>
+
+<style scoped>
+</style>
